a0ae61f5915875434912147563a5f96ab7b028a7
ldd911127 <365037133@qq.com>
2017-02-27 18:21:30 +0800

    添加时间转换函数

--- tool/convert.go
+++ tool/convert.go
@@ -1,6 +1,10 @@
 package tool
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+	"time"
+)
 
 func ToString(s interface{}) string {
 	switch vlu := s.(type) {
@@ -12,4 +16,27 @@ func ToString(s interface{}) string {
 		return vlu
 	}
 	return ""
+}
+
+func StrToTime(s string, format string) time.Time {
+	tempFormat := strings.Replace(format, "yyyy", "2006", -1)
+	tempFormat = strings.Replace(tempFormat, "MM", "01", -1)
+	tempFormat = strings.Replace(tempFormat, "dd", "02", -1)
+	tempFormat = strings.Replace(tempFormat, "HH", "15", -1)
+	tempFormat = strings.Replace(tempFormat, "mm", "04", -1)
+	tempFormat = strings.Replace(tempFormat, "ss", "05", -1)
+	loc, _ := time.LoadLocation("Local")
+	formatTime, _ := time.ParseInLocation(tempFormat, s, loc)
+	return formatTime
+}
+
+func TimeToStr(t time.Time, format string) string {
+	tempFormat := strings.Replace(format, "yyyy", "2006", -1)
+	tempFormat = strings.Replace(tempFormat, "MM", "01", -1)
+	tempFormat = strings.Replace(tempFormat, "dd", "02", -1)
+	tempFormat = strings.Replace(tempFormat, "HH", "15", -1)
+	tempFormat = strings.Replace(tempFormat, "mm", "04", -1)
+	tempFormat = strings.Replace(tempFormat, "ss", "05", -1)
+	formatTime := t.Format(tempFormat)
+	return formatTime
 }
