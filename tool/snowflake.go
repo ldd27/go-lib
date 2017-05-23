9dc8ed24f0d1dab52042d60b92de40f6f483f6f5
ldd911127 <365037133@qq.com>
2017-05-23 17:58:00 +0800

    更换snowflake库

--- tool/snowflake.go
+++ tool/snowflake.go
@@ -1,24 +1,33 @@
 package tool
 
-import "github.com/zheng-ji/goSnowFlake"
+import "github.com/bwmarrin/snowflake"
 
-func GenNewID() (int64, error) {
-	iw, err := goSnowFlake.NewIdWorker(1)
+var node *snowflake.Node
+
+func init() {
+	var err error
+	node, err = snowflake.NewNode(1)
 	if err != nil {
-		return 0, err
-	}
-	if id, err := iw.NextId(); err != nil {
-		return 0, err
-	} else {
-		return id, nil
+		panic(err)
 	}
 }
 
+//func GenNewID() (int64, error) {
+//iw, err := goSnowFlake.NewIdWorker(1)
+//if err != nil {
+//	return 0, err
+//}
+//if id, err := iw.NextId(); err != nil {
+//	return 0, err
+//} else {
+//	return id, nil
+//}
+//}
+
 func NewID() int64 {
-	newid, _ := GenNewID()
-	return newid
+	return node.Generate().Int64()
 }
 
 func NewStrID() string {
-	return ToString(NewID())
+	return node.Generate().String()
 }
